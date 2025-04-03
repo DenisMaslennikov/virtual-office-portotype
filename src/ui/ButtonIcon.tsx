fa9fdeba151103588b43e91fed37ede0233b9dfb
Denis Maslennikov <ktotom7@yandex.ru>
2025-04-03 18:26:15 +0300

    Added ButtonIcon

---
+++ src/ui/ButtonIcon.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const ButtonIcon = styled.button`
+  background: none;
+  border: none;
+
+  & svg {
+    width: 2.2rem;
+    height: 2.2rem;
+    color: var(--color-grey-500);
+  }
+`;
+
+export default ButtonIcon;
